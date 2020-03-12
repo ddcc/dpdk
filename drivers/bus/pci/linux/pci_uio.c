f859325482bd3870eb097f085f87c237e4e37ed9
Dominic Chen <d.c.ddcc@gmail.com>
2020-03-11 21:38:47 -0400

    pci: define port i/o asm intrinsics

--- drivers/bus/pci/linux/pci_uio.c
+++ drivers/bus/pci/linux/pci_uio.c
@@ -12,10 +12,6 @@
 #include <sys/sysmacros.h>
 #include <linux/pci_regs.h>
 
-#if defined(RTE_ARCH_X86)
-#include <sys/io.h>
-#endif
-
 #include <rte_string_fns.h>
 #include <rte_log.h>
 #include <rte_pci.h>
@@ -29,6 +25,55 @@
 void *pci_map_addr = NULL;
 
 #define OFF_MAX              ((uint64_t)(off_t)-1)
+
+#if defined(RTE_ARCH_X86)
+static __inline unsigned short int
+inw (unsigned short int __port)
+{
+  unsigned short _v;
+
+  __asm__ __volatile__ ("inw %w1,%0":"=a" (_v):"Nd" (__port));
+  return _v;
+}
+
+static __inline unsigned int
+inl (unsigned short int __port)
+{
+  unsigned int _v;
+
+  __asm__ __volatile__ ("inl %w1,%0":"=a" (_v):"Nd" (__port));
+  return _v;
+}
+
+static __inline unsigned char
+inb (unsigned short int __port)
+{
+  unsigned char _v;
+  __asm__ __volatile__ ("inb %w1,%0":"=a" (_v):"Nd" (__port));
+  return _v;
+}
+
+static __inline void
+outw_p (unsigned short int __value, unsigned short int __port)
+{
+  __asm__ __volatile__ ("outw %w0,%w1\noutb %%al,$0x80": :"a" (__value),
+                        "Nd" (__port));
+}
+
+static __inline void
+outl_p (unsigned int __value, unsigned short int __port)
+{
+  __asm__ __volatile__ ("outl %0,%w1\noutb %%al,$0x80": :"a" (__value),
+                        "Nd" (__port));
+}
+
+static __inline void
+outb_p (unsigned char __value, unsigned short int __port)
+{
+  __asm__ __volatile__ ("outb %b0,%w1\noutb %%al,$0x80": :"a" (__value),
+                        "Nd" (__port));
+}
+#endif
 
 int
 pci_uio_read_config(const struct rte_intr_handle *intr_handle,
